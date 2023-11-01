0dd402548397f75bd9d19a06f6c0a8bdc8cf228e
Nathan Peters <peters.nate8@gmail.com>
2023-10-31 19:33:43 -0600

    create Task component that renders a task on DOM, and display each task from API

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 const checkStatus = (response) => {
-  if(response.ok) {
+  if (response.ok) {
     return response;
   }
   throw new Error('Request either 404 or 500');
-}
+};
 
 const json = (response) => response.json();
+
+// Task component renders an individual task fetched from API
+class Task extends React.Component {
+  render() {
+    const { task, onDelete, onComplete } = this.props;
+    const { id, content, completed } = task;
+    return (
+      <div className='row mb-1'>
+        <p className='col'>{content}</p>
+        <button onClick={() => onDelete(id)}>Delete</button>
+        <input
+          className='d-inline-block mt-2'
+          type='checkbox'
+          onChange={() => onComplete(id, completed)}
+          checked={completed}
+        />
+      </div>
+    );
+  }
+}
 
 // To Do List component stores basic layout of web page and input event listeners
 class ToDoList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       new_task: '',
-      tasks: []
-     }
+      tasks: [],
+    };
 
-     this.handleChange = this.handleChange.bind(this);
-     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   // runs after component has loaded
@@ -28,48 +48,55 @@ class ToDoList extends React.Component {
       .then(json)
       .then((response) => {
         console.log(response);
-        this.setState({tasks: response.tasks});
+        this.setState({ tasks: response.tasks });
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error.message);
-      })
+      });
   }
 
   handleChange(event) {
-    this.setState({ new_task: event.target.value});
+    this.setState({ new_task: event.target.value });
   }
 
   handleSubmit(event) {
     event.preventDefault();
   }
 
-  render() { 
+  render() {
     const { new_task, tasks } = this.state;
-    return ( 
+    return (
       <div className='container'>
         <div className='row'>
           <div className='col-12'>
             <h2 className='mb-3'>To Do List</h2>
-            {tasks.length > 0 ? tasks.map((task) => {
-              return null;
-            }) : <p>No tasks here</p>}
+            {tasks.length > 0 ? (
+              tasks.map((task) => {
+                // render each task
+                return <Task key={task.id} task={task}/>
+              })
+            ) : (
+              <p>No tasks here</p>
+            )}
             <form onSubmit={this.handleSubmit} className='form-inline my-4'>
-              <input 
+              <input
                 type='text'
                 className='form-control mr-sm-2 mb-2'
                 placeholder='New Task'
                 value={new_task}
                 onChange={this.handleChange}
               />
-              <button type='submit' className='btn btn-primary mb-2'>Submit</button>
+              <button type='submit' className='btn btn-primary mb-2'>
+                Submit
+              </button>
             </form>
           </div>
         </div>
       </div>
-     );
+    );
   }
 }
- 
+
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(<ToDoList />);
